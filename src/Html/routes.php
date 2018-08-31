50b46d2a4f0a676092eb4431335c8d4ef8646e86
Sitharaam Jayakumar <sr.jai.estatemate@gmail.com>
2018-08-31 16:55:32 +0530

    change in routes.php

--- src/Html/routes.php
+++ src/Html/routes.php
@@ -6,8 +6,8 @@ Route::get('geocode/test', function () {
     return 'support_resort_geocode_ip';
 });
 
-Route::get('geocode/{ip}/{apikey}/{format}', function () {
-    return Geocode::getGeoLocation($ip, $apikey, $format);
+Route::get('geocode/{ip}/{format}', function () {
+    return Geocode::getGeoLocation($ip, $format);
 });
 
 Route::get('geocode/config', function () {
